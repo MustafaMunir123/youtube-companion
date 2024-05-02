bf59a3792b58af0b8260e438b4b25b7f8d42768e
MustafaMunir123 <mustafamunir10@gmail.com>
2024-05-02 16:30:14 +0500

    feat: add utility to get video urls and titles from playlist

---
+++ playlist.py
@@ -0,0 +1,22 @@
+import pafy
+import json
+from pytube import Playlist, YouTube
+
+p = Playlist('https://youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n')
+
+data_dict = {}
+
+for video_url in p:
+    print(video_url)
+    video = YouTube(video_url)
+    # video = pafy.new(video_url)
+    print(video.title)
+    data_dict[video_url] = video.title
+    
+# with open("playlist_data.json", "w+") as file:
+#     file.write(json.dumps(data_dict))
+
+
+
+
+# https://www.youtube.com/watch?v=41qgdwd3zAg?t=13s
